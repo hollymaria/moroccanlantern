46f134cfe7b5bb4bba6ecfde266a7dd4edca99fb
Holly Maria <72448486+hollymaria@users.noreply.github.com>
2021-07-12 22:33:49 +0100

    Add files via upload

---
+++ moroccanlantern/script.js
@@ -0,0 +1,11 @@
+function light(value) {
+if(value == 0)
+{
+image="images/lantern off.png";
+
+} else {
+
+image="images/Lantern on (1).png";
+    }
+    document.getElementById('lantern').src=image;
+}
